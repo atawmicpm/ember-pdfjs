3e764b6666d02c8aaed034e698004618dd27792f
Phillip Mispagel <pmispagel@turnitin.com>
2016-01-30 15:42:38 -0800

    added extra acceptance test for testing purposes

--- tests/acceptance/user-can-view-pdf-test.js
+++ tests/acceptance/user-can-view-pdf-test.js
@@ -27,6 +27,38 @@ test('loads, scrolling causes new pages to render and old pages to expire from t
   waitForPromise(loaded);
 
   andThen(function() {
+
+    var $container = find('.pdf-document-container');
+    
+    assert.equal($container.children().length, 14); // 14 pdf-page components in DOM
+    assert.ok(!$container.children().first().is(':empty')); // make sure the first page has content
+
+    var lastPage = $container.children().last()[0];
+    lastPage.scrollIntoView();
+
+    var scrolled = application.__container__.lookup('component:pdf-document').get('componentScrolled');
+
+    waitForPromise(scrolled);
+
+    andThen(function() {
+      assert.ok($container.children().first().is(':empty')); // make sure the first page is empty
+      assert.ok(!$($container.children()[5]).is(':empty')); // make sure the 5th page has content
+    });
+  });
+});
+
+
+
+test('loads, scrolling causes new pages to render and old pages to expire from the DOM.', function(assert) {
+
+  visit('/');
+
+  // promise hooks that need to resolve inside pdf-document
+  var loaded = application.__container__.lookup('component:pdf-document').get('componentLoaded');
+  
+  waitForPromise(loaded);
+
+  andThen(function() {
     
     var $container = find('.pdf-document-container');
     
